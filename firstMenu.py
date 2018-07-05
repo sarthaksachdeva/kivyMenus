ae872ce5c68b0cd8e6d93dbcef4e35c8218d9a76
sarthaksachdeva <sachdevasarthak41@gmail.com>
2018-07-05 07:20:39 +0530

    initial commit

---
+++ firstMenu.py
@@ -0,0 +1,46 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ObjectProperty
+from kivy.uix.carousel import Carousel
+from kivy.uix.boxlayout import BoxLayout
+from kivyDemos.FirstMenuTst.texts import textArray, widgetDict
+from kivy.uix.dropdown import DropDown
+from kivy.uix.button import Button
+
+
+class rootWidget(FloatLayout):
+    boxes = ObjectProperty(None)
+    navBar = ObjectProperty(None)
+
+    def getText(self,id):
+        return textArray[widgetDict[id]]
+
+class bxL(BoxLayout):
+    def getText(self,id):
+        return textArray[widgetDict[id]]
+
+
+class CustomDropDown(DropDown):
+    pass
+
+class DemoApp(App):
+    def build(self):
+        root = Builder.load_file('root.kv')
+        layout1 = Builder.load_file('layout.kv')
+        layout2 = Builder.load_file('layout2.kv')
+        aboutMenu = Builder.load_file('about.kv')
+
+        mainbutton = Button(text=root.getText('about'))
+        mainbutton.bind(on_release=aboutMenu.open)
+        aboutMenu.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
+
+        # print(len(root.navBar.children))
+        root.navBar.add_widget(mainbutton)
+        root.boxes.add_widget(layout1)
+        root.boxes.add_widget(layout2)
+        return root
+
+
+if __name__ == '__main__':
+    DemoApp().run()
---
+++ texts.py
@@ -0,0 +1,36 @@
+textArray = [None]*50
+textArray[1]  = '='
+textArray[2]  = 'Operation'
+textArray[3]  = 'Report'
+textArray[4]  = 'Config'
+textArray[5]  = 'About'
+textArray[6]  = 'Operation\n\n\nInbound'
+textArray[7]  = 'Operation\n\n\nOutbound - Local'
+textArray[8]  = 'Operation\n\n\nOutbound - Union'
+textArray[9]  = 'Report\n\n\nInbound - Daily'
+textArray[10] = 'Report\n\n\nInbound - Monthly'
+textArray[11] = 'Report\n\n\nInbound - Annual'
+textArray[12] = 'Report\n\n\nInbound - Customer'
+textArray[13] = 'Report\n\n\nOutbound - Daily'
+textArray[14] = 'Report\n\n\nOutbound - Monthly'
+textArray[15] = 'Report\n\n\nOutbound - Annual'
+textArray[16] = 'Report\n\n\nStock'
+
+widgetDict = {
+    'burger': 1,
+    'opertn': 2,
+    'report': 3,
+    'config': 4,
+    'about' : 5,
+    'oprtn1': 6,
+    'oprtn2': 7,
+    'oprtn3': 8,
+    'rpindl': 9,
+    'rpinmn': 10,
+    'rpinan': 11,
+    'rpinct': 12,
+    'rpotdl': 13,
+    'rpotmn': 14,
+    'rpotan': 15,
+    'rpotst': 16,
+}
